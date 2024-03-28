31431661210152b3055f7387685f3cc927491a92
Deep <deepprajapati1010@gmail.com>
2024-03-28 12:36:12 -0400

    added back-to-top button functionality/event listener.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,23 @@ navToggler.addEventListener("click", function () {
   navbar.classList.toggle("active");
   this.classList.toggle("active");
 });
+
+/**
+ * HEADER & BACK TOP BTN
+ * header and back top btn visible when window scroll down to 200px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+};
+
+window.addEventListener("scroll", activeElementOnScroll);
