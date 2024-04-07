2d17e8f0287a3ea4b612ee65b1ae2a07daa3dd57
Monisha Ranjan <ranjan.monisha233@gmail.com>
2024-04-07 15:12:56 -0400

    Implementedd progress bar update logic

--- assets/js/signUp.js
+++ assets/js/signUp.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("step-form");
     const steps = Array.from(document.querySelectorAll(".step"));
+    const progressBar = document.getElementById("progress-bar");
+    const progressBarWidth = 100 / (steps.length - 1);
     let currentStep = 0;
-    
+
+    function updateProgressBar() {
+       
+        const progress = currentStep * progressBarWidth;
+        progressBar.style.width = progress + "%";
+      }
+      
 });
