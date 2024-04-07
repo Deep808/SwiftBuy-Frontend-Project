bedb6cac8fea48adfc477bd087d9ab65e44d5083
Monisha Ranjan <ranjan.monisha233@gmail.com>
2024-04-07 13:54:56 -0400

    Improve step container responsiveness for mobile screens  and Initialize form handling and step navigation

---
+++ assets/js/signUp.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("step-form");
+    const steps = Array.from(document.querySelectorAll(".step"));
+    let currentStep = 0;
+    
+});
